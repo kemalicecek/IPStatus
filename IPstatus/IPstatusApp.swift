b59d09f71099060568dc1836eead08633032826c
kemalicecek <eren@kemalicecek.com>
2021-07-31 23:11:26 +0300

    Initial Commit

---
+++ IPstatus/IPstatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPstatusApp.swift
+//  IPstatus
+//
+//  Created by Kemal Icecek on 31.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct IPstatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
